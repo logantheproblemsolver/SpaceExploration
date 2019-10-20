9388ac39df423cf03734e4432ce6cc94619ae139
Logan Ellis <mrloganellis@gmail.com>
2019-10-20 19:38:00 -0400

    got a lot of the code done, need to finish being able to display results

--- index.js
+++ index.js
@@ -1,48 +1,185 @@
-NASA_apiKey = ''
-Youtube_apiKey = ''
-youtubeSearchURL = ''
-NASASearchURL = ''
-
+NASA_apiKey = 'fVdyFifJQ5EW8GNgZfe6wZRIrGxIF23gePQIsltC'
+Youtube_apiKey = 'AIzaSyAvVMyc7xMckEifggvX6O3GXzr7DLxTe-w'
+youtubeSearchURL = 'https://www.googleapis.com/youtube/v3/search'
+NASASearchURL = 'https://images-api.nasa.gov/search'
+NASADateURL = 'https://api.nasa.gov/planetary/apod'
 
 // first set of JS will be for the search bar 
 // here is the 1st set
 function searchSubmit() {
-    // this will be for searching topics about space
+    $('#searchSubmit').on('click', function(s) {
+        console.log('searchSubmit ran!')
+        s.preventDefault();
+        let search = $('#searchInput').val();
+        console.log(search)
+        getSpaceSearchNASAApi(search);
+        getSpaceSearchYoutubeApi(search);
+ 
+    })
 }
 
 
-function formatSearchQueryParams() {
-    // this is to format the search queries
+function formatSearchQueryParams(params) {
+    const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+return queryItems.join('&');
 }
 
 
-function getSpaceSearch() {
-    // this is to send the API for the search params
+function getSpaceSearchNASAApi(search) {
+    const params = {
+        q: search,
+        media_type: "image",
+
+    }
+    const queryString = formatSearchQueryParams(params)
+    const searchURL = NASASearchURL + '?' + queryString;
+
+    fetch(searchURL) 
+        .then(response =>{
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseSpaceSearchJson => displaySpaceSearchResult(responseSpaceSearchJson))
+        .catch(err => console.log(err.message))
 }
 
 
-function displaySearchResult() {
-    // this is to display the search results 
+function getSpaceSearchYoutubeApi(search) {
+    const params = {
+        key: Youtube_apiKey,
+        q: search,
+        part: 'snippet',
+        };
+      const queryString = formatSearchQueryParams(params)
+      const url = youtubeSearchURL + '?' + queryString;
+        
+      
+      fetch(url, {mode: 'cors'})
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } 
+            throw new Error(response.statusText);
+        })
+        .then(responseYoutubeJson => displayYoutubeSearchResults(responseYoutubeJson))
+        .catch(err => console.log(err.message));
+      
 }
+
+
+function displaySpaceSearchResult(responseSpaceSearchJson) {
+
+    for (i=0; i < responseSpaceSearchJson.collection.items.length; i++) {
+        let picture = responseSpaceSearchJson.collection.items[i].links[0].href
+         $('#NASAResults').append(`
+         <li><img src="${picture}"></li>
+         `) 
+    }
+}
+
+
+function displayYoutubeSearchResults(responseYoutubeJson) {
+    console.log(responseYoutubeJson)
+    for (let i=0; i < responseYoutubeJson.items.length; i++) {
+        console.log(`${responseYoutubeJson.items[i].snippet.title}`)
+        console.log(`responseYoutubeJson.items[i].snippet.thumbnails.default.url`)
+        $('#youtubeSearchVideos').append( 
+            `<li>
+                <p class="title">${responseYoutubeJson.items[i].snippet.title}</p>
+                <img src='${responseYoutubeJson.items[i].snippet.thumbnails.default.url}'>
+            </li>`
+        )
+    }
+}
+
 
 // here is the 2nd set
 function dateSubmit() {
-    // this is for submiting the date for photo of the day
+    $('#dateSubmit').on('click', function(d) {
+        console.log('dateSubmit ran!')
+        d.preventDefault();
+        let date = $('#dateInput').val();
+        console.log(date)
+        getSpaceDate(date);
+        $('.dateResults').empty();
+    })
 }
 
 
-function formatDataQueryParams() {
-    // this is to format the date params
+function formatDateQueryParams(parameters) {
+    const queryItems = Object.keys(parameters)
+        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(parameters[key])}`)
+    return queryItems.join('&');
 }
 
 
-function getSpaceDate() {
-    // this is to fetch the API for the date
+function getSpaceDate(date) {
+    const parameters = {
+        api_key: NASA_apiKey,
+        date: date     
+    }
+
+    const queryString = formatDateQueryParams(parameters)
+    const url = NASADateURL + '?' + queryString;
+
+
+    fetch(url)
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        } 
+        throw new Error(response.statusText);
+    })
+    .then(responseNASADateJson => {
+        displayDateResults(responseNASADateJson)
+        getYoutubeDateResults(responseNASADateJson)
+    }
+        )
+    .catch(err => console.log(err.message));
+
+    
 }
 
 
-function displayDateResults() {
-    // this is to display the date results
+function getYoutubeDateResults(responseNASADateJson) {
+    let dateYoutubeVideo = responseNASADateJson.title;
+    const parameters = {
+        key: Youtube_apiKey,
+        q: dateYoutubeVideo,
+        part: 'snippet',
+    }
+
+
+    const queryString = formatDateQueryParams(parameters)
+    const url = youtubeSearchURL + '?' + queryString;
+
+    fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.StatusText)
+        })
+        .then(responseYoutubeDateResults => displayYoutubeDateResults(responseYoutubeDateResults))
+        .catch(err => console.log(err.message))
+}
+
+function displayDateResults(responseNASADateJson) {
+    let pictureDate = responseNASADateJson.url
+    $('#dateNASA').append(`<img src='${pictureDate}'>`)
+}
+
+
+function displayYoutubeDateResults(responseYoutubeDateResults) {
+    for (let i=0; i < responseYoutubeDateResults.items.length; i++) {
+        console.log(responseYoutubeDateResults.items[i].snippet.thumbnails.medium.url)
+        $('#dateYoutube').append( 
+            `<img src="responseYoutubeDateResults.items[i].snippet.thumbnails.medium.url"/>`
+        )
+    }
 }
 
 // end of the sets
